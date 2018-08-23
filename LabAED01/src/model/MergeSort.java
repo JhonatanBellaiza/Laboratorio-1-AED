1c77c1b839cbce799dc81284fb8f6875fe29a9cf
HP <jhonatan_bellaiza@hotmail.com>
2018-08-23 02:50:48 -0500

    takeTimesSort added

--- LabAED01/src/model/MergeSort.java
+++ LabAED01/src/model/MergeSort.java
@@ -2,27 +2,16 @@ package model;
 import java.util.Random;
 
 public class MergeSort {
-     
-	public static void main(String[] args){
-        final int SIZE = 10000;
+	
+	
+	
+    public MergeSort() {
+		
+	}
 
-        Random r = new Random();
-        int[] test = new int[SIZE];
-
-        for (int i = 0; i < SIZE; i++){
-            test[i] = r.nextInt(Integer.MAX_VALUE);
-        }
-
-        long start = System.currentTimeMillis();
-        mergesort(test, 0, test.length-1);
-        long end = System.currentTimeMillis();
-
-
-        System.out.println("Mayor: "+ test[test.length-1]);
-        System.out.println("MilliTimes: "+(end-start));
-    }
-     
-    public static void mergesort(int A[],int izq, int der){
+	public static void mergesort( int A[],int izq, int der){
+		
+	
       
     	if (izq<der){
         	int m=(izq+der)/2;
@@ -50,4 +39,27 @@ public class MergeSort {
     	             while (i<=m) 
     	                           A[k++]=B[i++];
     	 }
+    
+    
+    
+ 
+
+	public static void main(String[] args){
+        final int SIZE = 10000;
+
+        Random r = new Random();
+        int[] test = new int[SIZE];
+
+        for (int i = 0; i < SIZE; i++){
+            test[i] = r.nextInt(Integer.MAX_VALUE);
+        }
+
+        long start = System.currentTimeMillis();
+        mergesort(test, 0, test.length-1);
+        long end = System.currentTimeMillis();
+
+
+        System.out.println("Mayor: "+ test[test.length-1]);
+        System.out.println("MilliTimes: "+(end-start));
+    }
 }
--- LabAED01/src/model/Procesator.java
+++ LabAED01/src/model/Procesator.java
@@ -2,6 +2,15 @@ package model;
 
 public class Procesator {
 	
+	public final static double MS_PER_SECOND = 1000;
+	public final static double NS_PER_SECOND = 1000000000;
+
+	
+	private QuickSort quick;
+	private MergeSort merge;
+	private RadixSort radix;
+	
+	
 	
 	public Procesator() {
 		
@@ -9,9 +18,112 @@ public class Procesator {
 	}
 	
 	
-	public void calculateTime() {
+	public String takeTimeQuickSort(int A[], int izq, int der) {
 		
+		String time = "";
+		
+		//Milliseconds
+		long START_MS=System.currentTimeMillis();
+		getQuick().quicksort(A, izq, der);
+		long DURATION_MS = System.currentTimeMillis()-START_MS;
+		
+		//Nanoseconds
+		long START_NS = System.nanoTime();
+		getQuick().quicksort(A, izq, der);
+		long DURATION_NS = System.nanoTime()-START_NS;
+
+		//Seconds
+		
+		double DURATION_S1= DURATION_MS/MS_PER_SECOND;
+		double DURATION_S2= DURATION_NS/NS_PER_SECOND;
+
+		time+= "RESULT IN NANOSECONDS: "+DURATION_MS+"nanoseconds"+"\n"+
+				"RESULT IN SECONDS: "+DURATION_S2+"seconds";
+		return time;
+	}
+	
+	public String takeTimeMergeSort(int A[], int izq,int m, int der) {
+		
+		String time = "";
+		
+		//Milliseconds
+		long START_MS=System.currentTimeMillis();
+		getMerge().merge(A, izq, m, der);
+		long DURATION_MS = System.currentTimeMillis()-START_MS;
+		
+		//Nanoseconds
+		long START_NS = System.nanoTime();
+		getMerge().merge(A, izq, m, der);
+		long DURATION_NS = System.nanoTime()-START_NS;
+
+		//Seconds
+		
+		double DURATION_S1= DURATION_MS/MS_PER_SECOND;
+		double DURATION_S2= DURATION_NS/NS_PER_SECOND;
+
+		time+= "RESULT IN NANOSECONDS: "+DURATION_MS+"nanoseconds"+"\n"+
+				"RESULT IN SECONDS: "+DURATION_S2+"seconds";
+		return time;
+	}
+	
+	
+	public String takeTimeRadixSort(int A[]) {
+		
+		String time = "";
+		
+		//Milliseconds
+		long START_MS=System.currentTimeMillis();
+		getRadix().Radixsort(A);
+		long DURATION_MS = System.currentTimeMillis()-START_MS;
+		
+		//Nanoseconds
+		long START_NS = System.nanoTime();
+		getRadix().Radixsort(A);
+		long DURATION_NS = System.nanoTime()-START_NS;
+
+		//Seconds
+		
+		double DURATION_S1= DURATION_MS/MS_PER_SECOND;
+		double DURATION_S2= DURATION_NS/NS_PER_SECOND;
+
+		time+= "RESULT IN NANOSECONDS: "+DURATION_MS+"nanoseconds"+"\n"+
+				"RESULT IN SECONDS: "+DURATION_S2+"seconds";
+		return time;
+	}
+	
+	
+
+
+	public QuickSort getQuick() {
+		return quick;
 	}
 
+
+	public void setQuick(QuickSort quick) {
+		this.quick = quick;
+	}
+
+
+	public MergeSort getMerge() {
+		return merge;
+	}
+
+
+	public void setMerge(MergeSort merge) {
+		this.merge = merge;
+	}
+
+
+	public RadixSort getRadix() {
+		return radix;
+	}
+
+
+	public void setRadix(RadixSort radix) {
+		this.radix = radix;
+	}
+	
+	
+	
+
 }
-//pri=ueba
